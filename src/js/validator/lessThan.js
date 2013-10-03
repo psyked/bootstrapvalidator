d6d7187d0020cd8614193e098c611c6de362a53f
phuoc <phuoc@huuphuoc.me>
2013-10-03 09:42:48 +0700

    Add lessThan validator

---
+++ src/js/validator/lessThan.js
@@ -0,0 +1,21 @@
+(function($) {
+    $.extend($.bootstrapValidator.validator, {
+        lessThan: {
+            /**
+             * Return true if the input value is less than or equal to given number
+             *
+             * @param {bootstrapValidator} validateInstance Validate plugin instance
+             * @param {HTMLElement} element
+             * @param {Object} options Can consist of the following keys:
+             * - value: The number used to compare to
+             * - strict [optional]: Can be true or false. Default is true
+             * - message: The invalid message
+             * @returns {boolean}
+             */
+            validate: function(validateInstance, element, options) {
+                var value = parseFloat($(element).val());
+                return (options.strict === true) ? (value < options.value) : (value <= options.value);
+            }
+        }
+    });
+}(window.jQuery));
