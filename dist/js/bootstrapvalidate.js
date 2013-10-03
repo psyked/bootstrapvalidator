5bc5d45385dc7be2ad3c7c4c33f86f59c0784fe4
phuoc <phuoc@huuphuoc.me>
2013-10-03 14:38:17 +0700

    #13: Add US Zip Code validator

--- dist/js/bootstrapvalidate.js
+++ dist/js/bootstrapvalidate.js
@@ -474,3 +474,21 @@
         }
     });
 }(window.jQuery));
+;(function($) {
+    $.extend($.bootstrapValidator.validator, {
+        usZipCode: {
+            /**
+             * Return true if and only if the input value is a valid US zip code
+             *
+             * @param {bootstrapValidator} validateInstance Validate plugin instance
+             * @param {HTMLElement} element
+             * @param {Object} options
+             * @returns {boolean}
+             */
+            validate: function(validateInstance, element, options) {
+                var value = $(element).val();
+                return /^\d{5}([\-]\d{4})?$/.test(value);
+            }
+        }
+    });
+}(window.jQuery));
