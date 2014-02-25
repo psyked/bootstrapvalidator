30de8ac19ce8dbf26cbefda11df2778a28b06f30
iplus <iplus@mail.ru>
2014-02-25 14:12:54 +0500

    Update callback.js

--- src/js/validator/callback.js
+++ src/js/validator/callback.js
@@ -17,7 +17,7 @@
         validate: function(validator, $field, options) {
             var value = $field.val();
             if (options.callback && 'function' == typeof options.callback) {
-                return options.callback.call(this, value, this);
+                return options.callback.call(this, value, validator);
             }
             return true;
         }
