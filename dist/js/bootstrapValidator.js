f59c9bb62fa9f1e60cdc54e7cfe8d7373e5749df
nghuuphuoc <phuoc@huuphuoc.me>
2014-03-20 14:18:14 +0700

    #95: Add MAC address validator

--- dist/js/bootstrapValidator.js
+++ dist/js/bootstrapValidator.js
@@ -983,6 +983,27 @@
     };
 }(window.jQuery));
 ;(function($) {
+    $.fn.bootstrapValidator.validators.mac = {
+        /**
+         * Return true if the input value is a MAC address.
+         *
+         * @param {BootstrapValidator} validator The validator plugin instance
+         * @param {jQuery} $field Field element
+         * @param {Object} options Can consist of the following keys:
+         * - message: The invalid message
+         * @returns {Boolean}
+         */
+        validate: function(validator, $field, options) {
+            var value = $field.val();
+            if (value == '') {
+                return true;
+            }
+
+            return /^([0-9A-F]{2}[:-]){5}([0-9A-F]{2})$/.test(value);
+        }
+    };
+}(window.jQuery));
+;(function($) {
     $.fn.bootstrapValidator.validators.notEmpty = {
         /**
          * Check if input value is empty or not
