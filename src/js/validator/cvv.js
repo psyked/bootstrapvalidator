527cc14461c385d2dae5d4652d7c9579b5a4be4a
nghuuphuoc <phuoc@huuphuoc.me>
2014-03-20 15:13:31 +0700

    #97: Add CVV number validator

---
+++ src/js/validator/cvv.js
@@ -0,0 +1,21 @@
+(function($) {
+    $.fn.bootstrapValidator.validators.cvv = {
+        /**
+         * Return true if the input value is a valid CVV number.
+         *
+         * @param {BootstrapValidator} validator The validator plugin instance
+         * @param {jQuery} $field Field element
+         * @param {Object} options Can consist of the following keys:
+         * - message: The invalid message
+         * @returns {Boolean}
+         */
+        validate: function(validator, $field, options) {
+            var value = $field.val();
+            if (value == '') {
+                return true;
+            }
+
+            return /^[0-9]{3,4}$/.test(value);
+        }
+    };
+}(window.jQuery));
