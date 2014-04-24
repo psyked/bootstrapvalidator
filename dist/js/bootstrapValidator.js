79130e87b6fd443e7c4faada4c2678930d7693f0
nghuuphuoc <phuoc@huuphuoc.me>
2014-04-24 17:10:54 +0700

    #179: Validate Dutch VAT number

--- dist/js/bootstrapValidator.js
+++ dist/js/bootstrapValidator.js
@@ -3180,6 +3180,33 @@
         },
 
         /**
+         * Validate Dutch VAT number
+         * Examples:
+         * - Valid: NL004495445B01
+         * - Invalid: NL123456789B90
+         *
+         * @param {String} value VAT number
+         * @return {Boolean}
+         */
+        _nl: function(value) {
+            if (!/^NL[0-9]{9}B[0-9]{2}$/.test(value)) {
+               return false;
+            }
+            value = value.substr(2);
+            var sum    = 0,
+                weight = [9, 8, 7, 6, 5, 4, 3, 2];
+            for (var i = 0; i < 8; i++) {
+                sum += parseInt(value.charAt(i)) * weight[i];
+            }
+
+            sum = sum % 11;
+            if (sum > 9) {
+                sum = 0;
+            }
+            return (sum == value.substr(8, 1));
+        },
+
+        /**
          * Validate Polish VAT number
          * Examples:
          * - Valid: PL8567346215
