1f95120a954f2c93db251ba74e135fe484b609a6
Konstantin Borisov <smeagol74@gmail.com>
2014-05-10 00:55:05 +0700

    small bugfix

--- src/js/bootstrapValidator.js
+++ src/js/bootstrapValidator.js
@@ -691,7 +691,7 @@
                                         var display = $(this).css('display'), v = $(this).attr('data-bv-validator');
                                         return ('block' == display) || ($field.data('bv.result.' + v) != that.STATUS_VALID);
                                     }).length == 0);
-                    this.disableSubmitButtons(validField ? false : true);
+                    this.disableSubmitButtons(this.isValid() ? false : true);
                     if ($icon) {
                         $icon
                             .removeClass(this.options.feedbackIcons.invalid).removeClass(this.options.feedbackIcons.validating).removeClass(this.options.feedbackIcons.valid)
