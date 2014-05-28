5c9195e4b3f3fa8fb6a935158df164684ba356da
Johan Rönn <johan@webbutveckling.net>
2014-05-28 11:22:06 +0200

    Fixed GreaterThen, LessThan and Between

--- src/js/validator/between.js
+++ src/js/validator/between.js
@@ -37,9 +37,10 @@
             }
 
             value = parseFloat(value);
-            return (options.inclusive === true)
-                        ? (value > options.min && value < options.max)
-                        : (value >= options.min && value <= options.max);
+			return (options.inclusive === true || options.inclusive==undefined)
+				? (value >= options.min && value <= options.max)
+				: (value > options.min && value < options.max);
+			
         }
     };
 }(window.jQuery));
