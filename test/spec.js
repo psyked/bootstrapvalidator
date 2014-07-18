5ca8cbb313b3a494b1b0059fe84f9c40e9ad6181
troymccabe <troymccabe@gmail.com>
2014-07-18 10:49:29 -0500

    Adding MEID validator

--- test/spec.js
+++ test/spec.js
@@ -3561,20 +3561,154 @@ describe('lessThan', function() {
     });
 });
 
+describe('meid', function() {
+    beforeEach(function () {
+        $([
+            '<form class="form-horizontal" id="meidForm">',
+                '<div id="msg"></div>',
+                '<div class="form-group">',
+                    '<input type="text" name="meid" data-bv-meid />',
+                '</div>',
+            '</form>'
+        ].join('\n')).appendTo('body');
+
+        $('#meidForm').bootstrapValidator();
+
+        this.bv   = $('#meidForm').data('bootstrapValidator');
+        this.$meid = this.bv.getFieldElements('meid');
+    });
+
+    afterEach(function () {
+        $('#meidForm').bootstrapValidator('destroy').remove();
+    });
+
+    it('Valid MEID (14 hex, check digit)', function() {
+        this.bv.resetForm();
+        this.$meid.val('A00000049259B16');
+        this.bv.validate();
+        expect(this.bv.isValid()).toBeTruthy();
+    });
+
+    it('Valid MEID (14 hex, dashes, check digit)', function() {
+        this.bv.resetForm();
+        this.$meid.val('A0-000004-9259B1-6');
+        this.bv.validate();
+        expect(this.bv.isValid()).toBeTruthy();
+    });
+
+    it('Valid MEID (14 hex, spaces, check digit)', function() {
+        this.bv.resetForm();
+        this.$meid.val('A0 000004 9259B1 6');
+        this.bv.validate();
+        expect(this.bv.isValid()).toBeTruthy();
+    });
+
+    it('Valid MEID (18 dec, check digit)', function() {
+        this.bv.resetForm();
+        this.$meid.val('2936087365007037100');
+        this.bv.validate();
+        expect(this.bv.isValid()).toBeTruthy();
+    });
+
+    it('Valid MEID (18 dec, dashes, check digit)', function() {
+        this.bv.resetForm();
+        this.$meid.val('29360-87365-0070-3710-0');
+        this.bv.validate();
+        expect(this.bv.isValid()).toBeTruthy();
+    });
+
+    it('Valid MEID (18 dec, spaces, check digit)', function() {
+        this.bv.resetForm();
+        this.$meid.val('29360 87365 0070 3710 0');
+        this.bv.validate();
+        expect(this.bv.isValid()).toBeTruthy();
+    });
+
+    it('Valid MEID (14 hex)', function() {
+        this.bv.resetForm();
+        this.$meid.val('AF0123450ABCDE');
+        this.bv.validate();
+        expect(this.bv.isValid()).toBeTruthy();
+    });
+
+    it('Valid MEID (14 hex, dashes)', function() {
+        this.bv.resetForm();
+        this.$meid.val('AF-012345-0ABCDE');
+        this.bv.validate();
+        expect(this.bv.isValid()).toBeTruthy();
+    });
+
+    it('Valid MEID (14 hex, spaces)', function() {
+        this.bv.resetForm();
+        this.$meid.val('AF 012345 0ABCDE');
+        this.bv.validate();
+        expect(this.bv.isValid()).toBeTruthy();
+    });
+
+    it('Valid MEID (18 dec)', function() {
+        this.bv.resetForm();
+        this.$meid.val('293608736500703710');
+        this.bv.validate();
+        expect(this.bv.isValid()).toBeTruthy();
+    });
+
+    it('Valid MEID (18 dec, dashes)', function() {
+        this.bv.resetForm();
+        this.$meid.val('29360-87365-0070-3710');
+        this.bv.validate();
+        expect(this.bv.isValid()).toBeTruthy();
+    });
+
+    it('Valid MEID (18 dec, spaces)', function() {
+        this.bv.resetForm();
+        this.$meid.val('29360 87365 0070 3710');
+        this.bv.validate();
+        expect(this.bv.isValid()).toBeTruthy();
+    });
+
+    it('Invalid MEID (14 hex, bad check digit)', function() {
+        this.bv.resetForm();
+        this.$meid.val('A00000049259B15');
+        this.bv.validate();
+        expect(this.bv.isValid()).toBeFalsy();
+    });
+
+    it('Invalid MEID (13 hex)', function() {
+        this.bv.resetForm();
+        this.$meid.val('A00000049259B');
+        this.bv.validate();
+        expect(this.bv.isValid()).toBeFalsy();
+    });
+
+    it('Invalid MEID (18 dec, bad check digit)', function() {
+        this.bv.resetForm();
+        this.$meid.val('2936087365007037101');
+        this.bv.validate();
+        expect(this.bv.isValid()).toBeFalsy();
+    });
+
+    it('Invalid MEID (17 dec)', function() {
+        this.bv.resetForm();
+        this.$meid.val('29360873650070371');
+        this.bv.validate();
+        expect(this.bv.isValid()).toBeFalsy();
+    });
+});
+
 describe('uri', function() {
     beforeEach(function () {
         $([
             '<form class="form-horizontal" id="uriForm">',
-            '<div id="msg"></div>',
-            '<div class="form-group">',
-            '<input type="text" name="uri" data-bv-uri />',
-            '</div>',
+                '<div id="msg"></div>',
+                '<div class="form-group">',
+                    '<input type="text" name="uri" data-bv-uri />',
+                '</div>',
             '</form>'
         ].join('\n')).appendTo('body');
 
         $('#uriForm').bootstrapValidator();
 
-        this.bv    = $('#uriForm').data('bootstrapValidator');
+        this.bv   = $('#uriForm').data('bootstrapValidator');
         this.$uri = this.bv.getFieldElements('uri');
     });
 
