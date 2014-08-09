e15bd364b0ae2c08edf1bf274359dd7a75ff04ec
phuoc <phuoc@huuphuoc.me>
2014-08-09 21:13:11 +0700

    Cleanup

--- dist/css/bootstrapValidator.min.css
+++ dist/css/bootstrapValidator.min.css
@@ -2,7 +2,7 @@
  * BootstrapValidator (http://bootstrapvalidator.com)
  * The best jQuery plugin to validate form fields. Designed to use with Bootstrap 3
  *
- * @version     v0.5.1-dev, built on 2014-08-09 9:38:42 AM
+ * @version     v0.5.1-dev, built on 2014-08-09 9:11:56 PM
  * @author      https://twitter.com/nghuuphuoc
  * @copyright   (c) 2013 - 2014 Nguyen Huu Phuoc
  * @license     MIT
