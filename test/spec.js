d11685feec065d27877703b4433605c662abb762
Emil Rømer Christensen <christensen.emil@gmail.com>
2014-09-11 20:03:49 +0200

    Add test suite for keyword color validator

--- test/spec.js
+++ test/spec.js
@@ -4724,6 +4724,61 @@ describe('issn', function() {
     });
 });
 
+describe('keywordColor', function() {
+
+    beforeEach(function() {
+        var html = [
+            '<div class="container">',
+                '<form class="form-horizontal" id="keywordColorForm">',
+                    '<div class="form-group">',
+                        '<input type="text" name="keyword" data-bv-keywordcolor />',
+                    '</div>',
+                '</form>',
+            '</div>'
+        ].join('\n');
+
+        $(html).appendTo('body');
+        $('#keywordColorForm').bootstrapValidator();
+
+        this.bv          = $('#keywordColorForm').data('bootstrapValidator');
+        this.$keywordColor = this.bv.getFieldElements('keyword');
+    });
+
+    afterEach(function() {
+        $('#keywordColorForm').bootstrapValidator('destroy').parent().remove();
+    });
+
+    it('accept transparent', function() {
+        this.$keywordColor.val('transparent');
+        this.bv.validate();
+        expect(this.bv.isValidField('keyword')).toBeTruthy();
+    });
+
+    it('accept blueviolet', function() {
+        this.$keywordColor.val('transparent');
+        this.bv.validate();
+        expect(this.bv.isValidField('keyword')).toBeTruthy();
+    });
+
+    it('reject combined keywords', function() {
+        this.$keywordColor.val('blueviolet red');
+        this.bv.validate();
+        expect(this.bv.isValidField('keyword')).toEqual(false);
+    });
+
+   it('reject shady', function() {
+        this.$keywordColor.val('shady');
+        this.bv.validate();
+        expect(this.bv.isValidField('keyword')).toEqual(false);
+    });
+
+    it('reject blueish', function() {
+        this.$keywordColor.val('blueish');
+        this.bv.validate();
+        expect(this.bv.isValidField('keyword')).toEqual(false);
+    });
+});
+
 function lessThanCompare() {
     var compareTo = $('#lessThanForm').find('[name="maxAge"]').val();
     $('#msg').html('lessThanCompare() called; compare to ' + compareTo);
