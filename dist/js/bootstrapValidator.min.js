7d1709872e273c6595fa659fefe1f150690657d3
Emil Rømer Christensen <christensen.emil@gmail.com>
2014-09-11 16:11:25 +0200

    Add built files for the rgbColor and keywordColor commits

--- dist/js/bootstrapValidator.min.js
+++ dist/js/bootstrapValidator.min.js
@@ -2,7 +2,7 @@
  * BootstrapValidator (http://bootstrapvalidator.com)
  * The best jQuery plugin to validate form fields. Designed to use with Bootstrap 3
  *
- * @version     v0.5.2-dev, built on 2014-09-18 9:55:28 PM
+ * @version     v0.5.2-dev, built on 2014-09-18 9:57:05 PM
  * @author      https://twitter.com/nghuuphuoc
  * @copyright   (c) 2013 - 2014 Nguyen Huu Phuoc
  * @license     MIT
