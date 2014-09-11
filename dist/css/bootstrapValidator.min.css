898bc39195dc472e43dd0b688a1b6db448881956
Emil Rømer Christensen <christensen.emil@gmail.com>
2014-09-11 18:03:37 +0200

    Add validator and test suite for hsl() color validation

--- dist/css/bootstrapValidator.min.css
+++ dist/css/bootstrapValidator.min.css
@@ -2,7 +2,7 @@
  * BootstrapValidator (http://bootstrapvalidator.com)
  * The best jQuery plugin to validate form fields. Designed to use with Bootstrap 3
  *
- * @version     v0.5.2-dev, built on 2014-09-18 10:01:09 PM
+ * @version     v0.5.2-dev, built on 2014-09-18 10:01:56 PM
  * @author      https://twitter.com/nghuuphuoc
  * @copyright   (c) 2013 - 2014 Nguyen Huu Phuoc
  * @license     MIT
