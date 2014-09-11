52f17016624adb1e5090fa2fb7bc547010765883
Emil Rømer Christensen <christensen.emil@gmail.com>
2014-09-11 19:08:58 +0200

    Add validator and test suite for hsla() color validation

--- dist/css/bootstrapValidator.min.css
+++ dist/css/bootstrapValidator.min.css
@@ -19,6 +19,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
  * @version     v0.5.3-dev, built on 2014-10-06 8:02:42 AM
 =======
  * @version     v0.5.2-dev, built on 2014-09-18 9:55:01 PM
@@ -71,6 +72,9 @@
 =======
  * @version     v0.5.2-dev, built on 2014-09-18 10:03:43 PM
 >>>>>>> Fix bug in form names for test suites for rgb, rgba and hsl
+=======
+ * @version     v0.5.2-dev, built on 2014-09-18 10:03:49 PM
+>>>>>>> Add validator and test suite for hsla() color validation
  * @author      https://twitter.com/nghuuphuoc
  * @copyright   (c) 2013 - 2014 Nguyen Huu Phuoc
  * @license     MIT
