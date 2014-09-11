8aad8c942aa27ab06862d31569aac91910f91c34
Emil Rømer Christensen <christensen.emil@gmail.com>
2014-09-11 22:42:32 +0200

    Add validator and test suite for color validation

--- dist/js/bootstrapValidator.js
+++ dist/js/bootstrapValidator.js
@@ -11,6 +11,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
  * @version     v0.5.3-dev, built on 2014-10-06 8:02:42 AM
 =======
  * @version     v0.5.2-dev, built on 2014-09-18 9:55:01 PM
@@ -39,6 +40,9 @@
 =======
  * @version     v0.5.2-dev, built on 2014-09-18 9:57:40 PM
 >>>>>>> Add test suite for keyword color validator
+=======
+ * @version     v0.5.2-dev, built on 2014-09-18 9:57:46 PM
+>>>>>>> Add validator and test suite for color validation
  * @author      https://twitter.com/nghuuphuoc
  * @copyright   (c) 2013 - 2014 Nguyen Huu Phuoc
  * @license     MIT
@@ -2214,6 +2218,61 @@ if (typeof jQuery === 'undefined') {
             }
 
             return { valid: isValid, message: message };
+        }
+    };
+}(window.jQuery));
+;(function($) {
+    $.fn.bootstrapValidator.i18n.color = $.extend($.fn.bootstrapValidator.i18n.color || {}, {
+        'default': 'Please enter a valid color'
+    });
+
+    $.fn.bootstrapValidator.validators.color = {
+
+        /**
+         * Return true if the input value is a valid color
+         *
+         * @param {BootstrapValidator} validator The validator plugin instance
+         * @param {jQuery} $field Field element
+         * @param {Object} options Can consist of the following keys:
+         * - message: The invalid message
+         * - type: The Array of valid color types.fn
+         * @returns {Boolean}
+         */
+        validate: function(validator, $field, options) {
+            var value = $field.val();
+            if (value === '') {
+                return true;
+            }
+
+            if (options.hasOwnProperty('type') && options.type instanceof Array) { // If no options.type are set, run all of them
+                if ($.inArray('hex', options.type)) {
+                    return $.fn.bootstrapValidator.validators.hexColor.validate(validator, $field, options);
+                }
+                if ($.inArray('rgb', options.type)) {
+                    return $.fn.bootstrapValidator.validators.rgbColor.validate(validator, $field, options);
+                }
+                if ($.inArray('rgba', options.type)) {
+                    return $.fn.bootstrapValidator.validators.rgbaColor.validate(validator, $field, options);
+                }
+                if ($.inArray('hsl', options.type)) {
+                    return $.fn.bootstrapValidator.validators.hslColor.validate(validator, $field, options);
+                }
+                if ($.inArray('hsla', options.type)) {
+                    return $.fn.bootstrapValidator.validators.hslaColor.validate(validator, $field, options);
+                }
+                if ($.inArray('keyword', options.type)) {
+                    return $.fn.bootstrapValidator.validators.keywordColor.validate(validator, $field, options);
+                }
+            } else {
+                return $.fn.bootstrapValidator.validators.hexColor.validate(validator, $field, options)
+                    || $.fn.bootstrapValidator.validators.rgbColor.validate(validator, $field, options)
+                    || $.fn.bootstrapValidator.validators.rgbaColor.validate(validator, $field, options)
+                    || $.fn.bootstrapValidator.validators.hslColor.validate(validator, $field, options)
+                    || $.fn.bootstrapValidator.validators.hslaColor.validate(validator, $field, options)
+                    || $.fn.bootstrapValidator.validators.keywordColor.validate(validator, $field, options);
+            }
+
+            return false;
         }
     };
 }(window.jQuery));
