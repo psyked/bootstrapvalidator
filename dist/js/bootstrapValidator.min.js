966ab4407150a646a2285527a46db6e9ea69ff6c
Emil Rømer Christensen <christensen.emil@gmail.com>
2014-09-11 20:03:49 +0200

    Add test suite for keyword color validator

--- dist/js/bootstrapValidator.min.js
+++ dist/js/bootstrapValidator.min.js
@@ -10,6 +10,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
  * @version     v0.5.3-dev, built on 2014-10-06 8:02:42 AM
 =======
  * @version     v0.5.2-dev, built on 2014-09-18 9:55:01 PM
@@ -35,6 +36,9 @@
 =======
  * @version     v0.5.2-dev, built on 2014-09-18 9:57:34 PM
 >>>>>>> Add validator and test suite for hsla() color validation
+=======
+ * @version     v0.5.2-dev, built on 2014-09-18 9:57:40 PM
+>>>>>>> Add test suite for keyword color validator
  * @author      https://twitter.com/nghuuphuoc
  * @copyright   (c) 2013 - 2014 Nguyen Huu Phuoc
  * @license     MIT
