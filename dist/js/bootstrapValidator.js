aba7318b8a13cc4ce92306e9e2f4dedfa0d8c78d
Emil Rømer Christensen <christensen.emil@gmail.com>
2014-09-11 18:03:37 +0200

    Add validator and test suite for hsl() color validation

--- dist/js/bootstrapValidator.js
+++ dist/js/bootstrapValidator.js
@@ -7,6 +7,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
  * @version     v0.5.3-dev, built on 2014-10-06 8:02:42 AM
 =======
  * @version     v0.5.2-dev, built on 2014-09-18 9:55:01 PM
@@ -23,6 +24,9 @@
 =======
  * @version     v0.5.2-dev, built on 2014-09-18 9:57:12 PM
 >>>>>>> Add validator and test suite for rgba() color validation
+=======
+ * @version     v0.5.2-dev, built on 2014-09-18 9:57:19 PM
+>>>>>>> Add validator and test suite for hsl() color validation
  * @author      https://twitter.com/nghuuphuoc
  * @copyright   (c) 2013 - 2014 Nguyen Huu Phuoc
  * @license     MIT
