7f7a72488f9b3bea49d087e06b3ed9dc3b064fe7
Phuoc Nguyen <phuoc@huuphuoc.me>
2014-09-12 09:28:11 +0700

    #787: Update Thai language package (Change the header of file)

--- dist/js/language/th_TH.js
+++ dist/js/language/th_TH.js
@@ -1,7 +1,7 @@
 (function($) {
     /**
-     * Default English package. It's included in the dist, so you do NOT need to include it to your head tag
-     * The only reason I put it here is that you can clone it, and translate it into your language
+     * Thai language package
+     * Translated by @figgaro
      */
     $.fn.bootstrapValidator.i18n = $.extend(true, $.fn.bootstrapValidator.i18n, {
         base64: {
