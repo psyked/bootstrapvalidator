236c33fe9eecdc3c8f04b34f867767457b57ef1f
Emil Rømer Christensen <christensen.emil@gmail.com>
2014-09-13 00:37:25 +0200

    Refactor color validators to one single entity

--- dist/js/bootstrapValidator.js
+++ dist/js/bootstrapValidator.js
@@ -2,7 +2,7 @@
  * BootstrapValidator (http://bootstrapvalidator.com)
  * The best jQuery plugin to validate form fields. Designed to use with Bootstrap 3
  *
- * @version     v0.5.2-dev, built on 2014-09-18 10:04:01 PM
+ * @version     v0.5.2-dev, built on 2014-09-18 10:04:11 PM
  * @author      https://twitter.com/nghuuphuoc
  * @copyright   (c) 2013 - 2014 Nguyen Huu Phuoc
  * @license     MIT
@@ -2206,33 +2206,86 @@ if (typeof jQuery === 'undefined') {
 
             if (options.hasOwnProperty('type') && options.type instanceof Array) { // If no options.type are set, run all of them
                 if ($.inArray('hex', options.type)) {
-                    return $.fn.bootstrapValidator.validators.hexColor.validate(validator, $field, options);
+                    return this._hexColor(validator, $field, options);
                 }
                 if ($.inArray('rgb', options.type)) {
-                    return $.fn.bootstrapValidator.validators.rgbColor.validate(validator, $field, options);
+                    return this._rgbColor(validator, $field, options);
                 }
                 if ($.inArray('rgba', options.type)) {
-                    return $.fn.bootstrapValidator.validators.rgbaColor.validate(validator, $field, options);
+                    return this._rgbaColor(validator, $field, options);
                 }
                 if ($.inArray('hsl', options.type)) {
-                    return $.fn.bootstrapValidator.validators.hslColor.validate(validator, $field, options);
+                    return this._hslColor(validator, $field, options);
                 }
                 if ($.inArray('hsla', options.type)) {
-                    return $.fn.bootstrapValidator.validators.hslaColor.validate(validator, $field, options);
+                    return this._hslaColor(validator, $field, options);
                 }
                 if ($.inArray('keyword', options.type)) {
-                    return $.fn.bootstrapValidator.validators.keywordColor.validate(validator, $field, options);
+                    return this._keywordColor(validator, $field, options);
                 }
             } else {
-                return $.fn.bootstrapValidator.validators.hexColor.validate(validator, $field, options)
-                    || $.fn.bootstrapValidator.validators.rgbColor.validate(validator, $field, options)
-                    || $.fn.bootstrapValidator.validators.rgbaColor.validate(validator, $field, options)
-                    || $.fn.bootstrapValidator.validators.hslColor.validate(validator, $field, options)
-                    || $.fn.bootstrapValidator.validators.hslaColor.validate(validator, $field, options)
-                    || $.fn.bootstrapValidator.validators.keywordColor.validate(validator, $field, options);
+                return this._hexColor(validator, $field, options)
+                    || this._rgbColor(validator, $field, options)
+                    || this._rgbaColor(validator, $field, options)
+                    || this._hslColor(validator, $field, options)
+                    || this._hslaColor(validator, $field, options)
+                    || this._keywordColor(validator, $field, options);
             }
 
             return false;
+        },
+
+        _hexColor: function(validator, $field, options) {
+            var value = $field.val();
+            if (value === '') {
+                return true;
+            }
+            return /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(value);
+        },
+
+        _hslColor: function(validator, $field, options) {
+            var value = $field.val();
+            if (value === '') {
+                return true;
+            }
+            return /^hsl\((\s*(-?\d+)\s*,)(\s*(\b(0?\d{1,2}|100)\b%)\s*,)(\s*(\b(0?\d{1,2}|100)\b%)\s*)\)$/.test(value);
+        },
+
+        _hslaColor: function(validator, $field, options) {
+            var value = $field.val();
+            if (value === '') {
+                return true;
+            }
+            return /^hsla\((\s*(-?\d+)\s*,)(\s*(\b(0?\d{1,2}|100)\b%)\s*,){2}(\s*(0?(\.\d+)?|1(\.0+)?)\s*)\)$/.test(value);
+        },
+
+        _keywordColor: function(validator, $field, options) {
+            var value = $field.val();
+            var colorKeywords = ["transparent", "aliceblue","antiquewhite","aqua","aquamarine","azure","beige","bisque","black","blanchedalmond","blue","blueviolet","brown","burlywood","cadetblue","chartreuse","chocolate","coral","cornflowerblue","cornsilk","crimson","cyan","darkblue","darkcyan","darkgoldenrod","darkgray","darkgreen","darkgrey","darkkhaki","darkmagenta","darkolivegreen","darkorange","darkorchid","darkred","darksalmon","darkseagreen","darkslateblue","darkslategray","darkslategrey","darkturquoise","darkviolet","deeppink","deepskyblue","dimgray","dimgrey","dodgerblue","firebrick","floralwhite","forestgreen","fuchsia","gainsboro","ghostwhite","gold","goldenrod","gray","green","greenyellow","grey","honeydew","hotpink","indianred","indigo","ivory","khaki","lavender","lavenderblush","lawngreen","lemonchiffon","lightblue","lightcoral","lightcyan","lightgoldenrodyellow","lightgray","lightgreen","lightgrey","lightpink","lightsalmon","lightseagreen","lightskyblue","lightslategray","lightslategrey","lightsteelblue","lightyellow","lime","limegreen","linen","magenta","maroon","mediumaquamarine","mediumblue","mediumorchid","mediumpurple","mediumseagreen","mediumslateblue","mediumspringgreen","mediumturquoise","mediumvioletred","midnightblue","mintcream","mistyrose","moccasin","navajowhite","navy","oldlace","olive","olivedrab","orange","orangered","orchid","palegoldenrod","palegreen","paleturquoise","palevioletred","papayawhip","peachpuff","peru","pink","plum","powderblue","purple","red","rosybrown","royalblue","saddlebrown","salmon","sandybrown","seagreen","seashell","sienna","silver","skyblue","slateblue","slategray","slategrey","snow","springgreen","steelblue","tan","teal","thistle","tomato","turquoise","violet","wheat","white","whitesmoke","yellow","yellowgreen"];
+            if (value === '') {
+                return true;
+            }
+            return $.inArray(value, colorKeywords) >= 0;
+        },
+
+        _rgbColor: function(validator, $field, options) {
+            var value = $field.val();
+            var regexInteger = /^rgb\((\s*(\b([01]?\d{1,2}|2[0-4]\d|25[0-5])\b)\s*,){2}(\s*(\b([01]?\d{1,2}|2[0-4]\d|25[0-5])\b)\s*)\)$/;
+            var regexPercent = /^rgb\((\s*(\b(0?\d{1,2}|100)\b%)\s*,){2}(\s*(\b(0?\d{1,2}|100)\b%)\s*)\)$/;
+            if (value === '') {
+                return true;
+            }
+            return regexInteger.test(value) || regexPercent.test(value);
+        },
+
+        _rgbaColor: function(validator, $field, options) {
+            var value = $field.val();
+            var regexInteger = /^rgba\((\s*(\b([01]?\d{1,2}|2[0-4]\d|25[0-5])\b)\s*,){3}(\s*(0?(\.\d+)?|1(\.0+)?)\s*)\)$/;
+            var regexPercent = /^rgba\((\s*(\b(0?\d{1,2}|100)\b%)\s*,){3}(\s*(0?(\.\d+)?|1(\.0+)?)\s*)\)$/;
+            if (value === '') {
+                return true;
+            }
+            return regexInteger.test(value) || regexPercent.test(value);
         }
     };
 }(window.jQuery));
@@ -3043,86 +3096,6 @@ if (typeof jQuery === 'undefined') {
         }
     };
 }(window.jQuery));
-;(function($) {
-    $.fn.bootstrapValidator.i18n.hexColor = $.extend($.fn.bootstrapValidator.i18n.hexColor || {}, {
-        'default': 'Please enter a valid hex color'
-    });
-
-    $.fn.bootstrapValidator.validators.hexColor = {
-        enableByHtml5: function($field) {
-            return ('color' === $field.attr('type'));
-        },
-
-        /**
-         * Return true if the input value is a valid hex color
-         *
-         * @param {BootstrapValidator} validator The validator plugin instance
-         * @param {jQuery} $field Field element
-         * @param {Object} options Can consist of the following keys:
-         * - message: The invalid message
-         * @returns {Boolean}
-         */
-        validate: function(validator, $field, options) {
-            var value = $field.val();
-            if (value === '') {
-                return true;
-            }
-            return /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(value);
-        }
-    };
-}(window.jQuery));
-;(function($) {
-    $.fn.bootstrapValidator.i18n.hslColor = $.extend($.fn.bootstrapValidator.i18n.hslColor || {}, {
-        'default': 'Please enter a valid hsl color'
-    });
-
-    $.fn.bootstrapValidator.validators.hslColor = {
-
-        /**
-         * Return true if the input value is a valid hsl() color
-         *
-         * @param {BootstrapValidator} validator The validator plugin instance
-         * @param {jQuery} $field Field element
-         * @param {Object} options Can consist of the following keys:
-         * - message: The invalid message
-         * @returns {Boolean}
-         */
-        validate: function(validator, $field, options) {
-            var value = $field.val();
-            if (value === '') {
-                return true;
-            }
-            return /^hsl\((\s*(-?\d+)\s*,)(\s*(\b(0?\d{1,2}|100)\b%)\s*,)(\s*(\b(0?\d{1,2}|100)\b%)\s*)\)$/.test(value);
-        }
-    };
-}(window.jQuery));
-
-;(function($) {
-    $.fn.bootstrapValidator.i18n.hslaColor = $.extend($.fn.bootstrapValidator.i18n.hslaColor || {}, {
-        'default': 'Please enter a valid hsla color'
-    });
-
-    $.fn.bootstrapValidator.validators.hslaColor = {
-
-        /**
-         * Return true if the input value is a valid hsla() color
-         *
-         * @param {BootstrapValidator} validator The validator plugin instance
-         * @param {jQuery} $field Field element
-         * @param {Object} options Can consist of the following keys:
-         * - message: The invalid message
-         * @returns {Boolean}
-         */
-        validate: function(validator, $field, options) {
-            var value = $field.val();
-            if (value === '') {
-                return true;
-            }
-            return /^hsla\((\s*(-?\d+)\s*,)(\s*(\b(0?\d{1,2}|100)\b%)\s*,){2}(\s*(0?(\.\d+)?|1(\.0+)?)\s*)\)$/.test(value);
-        }
-    };
-}(window.jQuery));
-
 ;(function($) {
     $.fn.bootstrapValidator.i18n.iban = $.extend($.fn.bootstrapValidator.i18n.iban || {}, {
         'default': 'Please enter a valid IBAN number',
@@ -5219,31 +5192,6 @@ if (typeof jQuery === 'undefined') {
     };
 }(window.jQuery));
 ;(function($) {
-    $.fn.bootstrapValidator.i18n.keywordColor = $.extend($.fn.bootstrapValidator.i18n.keywordColor || {}, {
-        'default': 'Please enter a valid color keyword'
-    });
-
-    $.fn.bootstrapValidator.validators.keywordColor = {
-        /**
-         * Return true if the input value is a valid keyword color
-         *
-         * @param {BootstrapValidator} validator The validator plugin instance
-         * @param {jQuery} $field Field element
-         * @param {Object} options Can consist of the following keys:
-         * - message: The invalid message
-         * @returns {Boolean}
-         */
-        validate: function(validator, $field, options) {
-            var value = $field.val();
-            var colorKeywords = ["transparent", "aliceblue","antiquewhite","aqua","aquamarine","azure","beige","bisque","black","blanchedalmond","blue","blueviolet","brown","burlywood","cadetblue","chartreuse","chocolate","coral","cornflowerblue","cornsilk","crimson","cyan","darkblue","darkcyan","darkgoldenrod","darkgray","darkgreen","darkgrey","darkkhaki","darkmagenta","darkolivegreen","darkorange","darkorchid","darkred","darksalmon","darkseagreen","darkslateblue","darkslategray","darkslategrey","darkturquoise","darkviolet","deeppink","deepskyblue","dimgray","dimgrey","dodgerblue","firebrick","floralwhite","forestgreen","fuchsia","gainsboro","ghostwhite","gold","goldenrod","gray","green","greenyellow","grey","honeydew","hotpink","indianred","indigo","ivory","khaki","lavender","lavenderblush","lawngreen","lemonchiffon","lightblue","lightcoral","lightcyan","lightgoldenrodyellow","lightgray","lightgreen","lightgrey","lightpink","lightsalmon","lightseagreen","lightskyblue","lightslategray","lightslategrey","lightsteelblue","lightyellow","lime","limegreen","linen","magenta","maroon","mediumaquamarine","mediumblue","mediumorchid","mediumpurple","mediumseagreen","mediumslateblue","mediumspringgreen","mediumturquoise","mediumvioletred","midnightblue","mintcream","mistyrose","moccasin","navajowhite","navy","oldlace","olive","olivedrab","orange","orangered","orchid","palegoldenrod","palegreen","paleturquoise","palevioletred","papayawhip","peachpuff","peru","pink","plum","powderblue","purple","red","rosybrown","royalblue","saddlebrown","salmon","sandybrown","seagreen","seashell","sienna","silver","skyblue","slateblue","slategray","slategrey","snow","springgreen","steelblue","tan","teal","thistle","tomato","turquoise","violet","wheat","white","whitesmoke","yellow","yellowgreen"];
-            if (value === '') {
-                return true;
-            }
-            return $.inArray(value, colorKeywords) >= 0;
-        }
-    };
-}(window.jQuery));
-;(function($) {
     $.fn.bootstrapValidator.i18n.lessThan = $.extend($.fn.bootstrapValidator.i18n.lessThan || {}, {
         'default': 'Please enter a value less than or equal to %s',
         notInclusive: 'Please enter a value less than %s'
@@ -5800,62 +5748,6 @@ if (typeof jQuery === 'undefined') {
         }
     };
 }(window.jQuery));
-;(function($) {
-    $.fn.bootstrapValidator.i18n.rgbColor = $.extend($.fn.bootstrapValidator.i18n.rgbColor || {}, {
-        'default': 'Please enter a valid rgb color'
-    });
-
-    $.fn.bootstrapValidator.validators.rgbColor = {
-
-        /**
-         * Return true if the input value is a valid rgb() color
-         *
-         * @param {BootstrapValidator} validator The validator plugin instance
-         * @param {jQuery} $field Field element
-         * @param {Object} options Can consist of the following keys:
-         * - message: The invalid message
-         * @returns {Boolean}
-         */
-        validate: function(validator, $field, options) {
-            var value = $field.val();
-            var regexInteger = /^rgb\((\s*(\b([01]?\d{1,2}|2[0-4]\d|25[0-5])\b)\s*,){2}(\s*(\b([01]?\d{1,2}|2[0-4]\d|25[0-5])\b)\s*)\)$/;
-            var regexPercent = /^rgb\((\s*(\b(0?\d{1,2}|100)\b%)\s*,){2}(\s*(\b(0?\d{1,2}|100)\b%)\s*)\)$/;
-            if (value === '') {
-                return true;
-            }
-            return regexInteger.test(value) || regexPercent.test(value);
-        }
-    };
-}(window.jQuery));
-
-;(function($) {
-    $.fn.bootstrapValidator.i18n.rgbaColor = $.extend($.fn.bootstrapValidator.i18n.rgbaColor || {}, {
-        'default': 'Please enter a valid rgb color'
-    });
-
-    $.fn.bootstrapValidator.validators.rgbaColor = {
-
-        /**
-         * Return true if the input value is a valid rgb() color
-         *
-         * @param {BootstrapValidator} validator The validator plugin instance
-         * @param {jQuery} $field Field element
-         * @param {Object} options Can consist of the following keys:
-         * - message: The invalid message
-         * @returns {Boolean}
-         */
-        validate: function(validator, $field, options) {
-            var value = $field.val();
-            var regexInteger = /^rgba\((\s*(\b([01]?\d{1,2}|2[0-4]\d|25[0-5])\b)\s*,){3}(\s*(0?(\.\d+)?|1(\.0+)?)\s*)\)$/;
-            var regexPercent = /^rgba\((\s*(\b(0?\d{1,2}|100)\b%)\s*,){3}(\s*(0?(\.\d+)?|1(\.0+)?)\s*)\)$/;
-            if (value === '') {
-                return true;
-            }
-            return regexInteger.test(value) || regexPercent.test(value);
-        }
-    };
-}(window.jQuery));
-
 ;(function($) {
     $.fn.bootstrapValidator.i18n.rtn = $.extend($.fn.bootstrapValidator.i18n.rtn || {}, {
         'default': 'Please enter a valid RTN number'
--- src/js/validator/color.js
+++ src/js/validator/color.js
@@ -23,33 +23,86 @@
 
             if (options.hasOwnProperty('type') && options.type instanceof Array) { // If no options.type are set, run all of them
                 if ($.inArray('hex', options.type)) {
-                    return $.fn.bootstrapValidator.validators.hexColor.validate(validator, $field, options);
+                    return this._hexColor(validator, $field, options);
                 }
                 if ($.inArray('rgb', options.type)) {
-                    return $.fn.bootstrapValidator.validators.rgbColor.validate(validator, $field, options);
+                    return this._rgbColor(validator, $field, options);
                 }
                 if ($.inArray('rgba', options.type)) {
-                    return $.fn.bootstrapValidator.validators.rgbaColor.validate(validator, $field, options);
+                    return this._rgbaColor(validator, $field, options);
                 }
                 if ($.inArray('hsl', options.type)) {
-                    return $.fn.bootstrapValidator.validators.hslColor.validate(validator, $field, options);
+                    return this._hslColor(validator, $field, options);
                 }
                 if ($.inArray('hsla', options.type)) {
-                    return $.fn.bootstrapValidator.validators.hslaColor.validate(validator, $field, options);
+                    return this._hslaColor(validator, $field, options);
                 }
                 if ($.inArray('keyword', options.type)) {
-                    return $.fn.bootstrapValidator.validators.keywordColor.validate(validator, $field, options);
+                    return this._keywordColor(validator, $field, options);
                 }
             } else {
-                return $.fn.bootstrapValidator.validators.hexColor.validate(validator, $field, options)
-                    || $.fn.bootstrapValidator.validators.rgbColor.validate(validator, $field, options)
-                    || $.fn.bootstrapValidator.validators.rgbaColor.validate(validator, $field, options)
-                    || $.fn.bootstrapValidator.validators.hslColor.validate(validator, $field, options)
-                    || $.fn.bootstrapValidator.validators.hslaColor.validate(validator, $field, options)
-                    || $.fn.bootstrapValidator.validators.keywordColor.validate(validator, $field, options);
+                return this._hexColor(validator, $field, options)
+                    || this._rgbColor(validator, $field, options)
+                    || this._rgbaColor(validator, $field, options)
+                    || this._hslColor(validator, $field, options)
+                    || this._hslaColor(validator, $field, options)
+                    || this._keywordColor(validator, $field, options);
             }
 
             return false;
+        },
+
+        _hexColor: function(validator, $field, options) {
+            var value = $field.val();
+            if (value === '') {
+                return true;
+            }
+            return /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(value);
+        },
+
+        _hslColor: function(validator, $field, options) {
+            var value = $field.val();
+            if (value === '') {
+                return true;
+            }
+            return /^hsl\((\s*(-?\d+)\s*,)(\s*(\b(0?\d{1,2}|100)\b%)\s*,)(\s*(\b(0?\d{1,2}|100)\b%)\s*)\)$/.test(value);
+        },
+
+        _hslaColor: function(validator, $field, options) {
+            var value = $field.val();
+            if (value === '') {
+                return true;
+            }
+            return /^hsla\((\s*(-?\d+)\s*,)(\s*(\b(0?\d{1,2}|100)\b%)\s*,){2}(\s*(0?(\.\d+)?|1(\.0+)?)\s*)\)$/.test(value);
+        },
+
+        _keywordColor: function(validator, $field, options) {
+            var value = $field.val();
+            var colorKeywords = ["transparent", "aliceblue","antiquewhite","aqua","aquamarine","azure","beige","bisque","black","blanchedalmond","blue","blueviolet","brown","burlywood","cadetblue","chartreuse","chocolate","coral","cornflowerblue","cornsilk","crimson","cyan","darkblue","darkcyan","darkgoldenrod","darkgray","darkgreen","darkgrey","darkkhaki","darkmagenta","darkolivegreen","darkorange","darkorchid","darkred","darksalmon","darkseagreen","darkslateblue","darkslategray","darkslategrey","darkturquoise","darkviolet","deeppink","deepskyblue","dimgray","dimgrey","dodgerblue","firebrick","floralwhite","forestgreen","fuchsia","gainsboro","ghostwhite","gold","goldenrod","gray","green","greenyellow","grey","honeydew","hotpink","indianred","indigo","ivory","khaki","lavender","lavenderblush","lawngreen","lemonchiffon","lightblue","lightcoral","lightcyan","lightgoldenrodyellow","lightgray","lightgreen","lightgrey","lightpink","lightsalmon","lightseagreen","lightskyblue","lightslategray","lightslategrey","lightsteelblue","lightyellow","lime","limegreen","linen","magenta","maroon","mediumaquamarine","mediumblue","mediumorchid","mediumpurple","mediumseagreen","mediumslateblue","mediumspringgreen","mediumturquoise","mediumvioletred","midnightblue","mintcream","mistyrose","moccasin","navajowhite","navy","oldlace","olive","olivedrab","orange","orangered","orchid","palegoldenrod","palegreen","paleturquoise","palevioletred","papayawhip","peachpuff","peru","pink","plum","powderblue","purple","red","rosybrown","royalblue","saddlebrown","salmon","sandybrown","seagreen","seashell","sienna","silver","skyblue","slateblue","slategray","slategrey","snow","springgreen","steelblue","tan","teal","thistle","tomato","turquoise","violet","wheat","white","whitesmoke","yellow","yellowgreen"];
+            if (value === '') {
+                return true;
+            }
+            return $.inArray(value, colorKeywords) >= 0;
+        },
+
+        _rgbColor: function(validator, $field, options) {
+            var value = $field.val();
+            var regexInteger = /^rgb\((\s*(\b([01]?\d{1,2}|2[0-4]\d|25[0-5])\b)\s*,){2}(\s*(\b([01]?\d{1,2}|2[0-4]\d|25[0-5])\b)\s*)\)$/;
+            var regexPercent = /^rgb\((\s*(\b(0?\d{1,2}|100)\b%)\s*,){2}(\s*(\b(0?\d{1,2}|100)\b%)\s*)\)$/;
+            if (value === '') {
+                return true;
+            }
+            return regexInteger.test(value) || regexPercent.test(value);
+        },
+
+        _rgbaColor: function(validator, $field, options) {
+            var value = $field.val();
+            var regexInteger = /^rgba\((\s*(\b([01]?\d{1,2}|2[0-4]\d|25[0-5])\b)\s*,){3}(\s*(0?(\.\d+)?|1(\.0+)?)\s*)\)$/;
+            var regexPercent = /^rgba\((\s*(\b(0?\d{1,2}|100)\b%)\s*,){3}(\s*(0?(\.\d+)?|1(\.0+)?)\s*)\)$/;
+            if (value === '') {
+                return true;
+            }
+            return regexInteger.test(value) || regexPercent.test(value);
         }
     };
 }(window.jQuery));
